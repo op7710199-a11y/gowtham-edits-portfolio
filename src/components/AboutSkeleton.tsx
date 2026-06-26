ba55147007714fc1aad836a57509347b346f7f0d
VINOD_NETHA23 <op7710199@gmail.com>
2026-06-26 08:20:51 +0530

    Create AboutSkeleton.tsx

---
+++ src/components/AboutSkeleton.tsx
@@ -0,0 +1,9 @@
+export function AboutSkeleton() {
+  return (
+    <section className="section-padding">
+      <div className="container-mx animate-pulse">
+        Loading...
+      </div>
+    </section>
+  );
+}
